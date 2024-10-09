7d8b83caa4b09f95302c89c5e14cc24b21b1b6ed
Richard765 <richardexposito04@gmail.com>
2024-10-09 19:17:42 +0200

    ACT3_2_2

--- Bloque1/Unidad3/ACT3_2/ACT3_2_2.java
+++ Bloque1/Unidad3/ACT3_2/ACT3_2_2.java
@@ -5,17 +5,25 @@ public class ACT3_2_2 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int num;
+		boolean esPrimo = true;
 		
 		System.out.print("Introduzca un numero: ");
 		num = scanner.nextInt();
 		
-		for (int i=2; i<num-1) {
-			esPrimo = true;
+		for (int i=2; i<num-1; i++) {
 			if(num%i==0) {
 				esPrimo = false;
 				break;
 			}
 		}
+		
+		if (esPrimo) {
+			System.out.println("El numero es primo");
+		} else {
+			System.out.println("El numero no es primo");
+		}
+		
 		scanner.close();
 	}
 }
+
