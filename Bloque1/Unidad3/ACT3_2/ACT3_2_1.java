2c0027aa44bec4b65f82a955b96be28276b2db30
Richard765 <richardexposito04@gmail.com>
2024-10-09 18:22:05 +0200

    Actividad 3_2

---
+++ Bloque1/Unidad3/ACT3_2/ACT3_2_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ACT3_2_1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num;
+		
+		System.out.print("Introduzca un numero: ");
+		num = scanner.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			if (num%i==0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
