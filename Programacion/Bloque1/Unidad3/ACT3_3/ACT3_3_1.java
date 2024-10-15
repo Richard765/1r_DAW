7db29b8abf5097af5b75c6a324d8be509b236d65
Richard765 <richardexposito04@gmail.com>
2024-10-15 16:48:31 +0200

    Ejercicio 3_3_1

--- Programacion/Bloque1/Unidad3/ACT3_3/ACT3_3_1.java
+++ Programacion/Bloque1/Unidad3/ACT3_3/ACT3_3_1.java
@@ -1,14 +1,14 @@
 public class ACT3_3_1 {
 
 	public static void main(String[] args) {
-		int num[] = new int[100];
+		int num[] = new int[10];
 		
 		for (int i=0; i<num.length;i++) {
 			num[i] = i + 1;
 		}
 		
-		for (int i = 0; i < num.length; i++) {
-			System.out.print(num[i] + " ");
+		for (int a : num) {
+			System.out.print(a + " ");
 		}
 	}
 }
