90b0cbf373ed39ce5063cbde074db80c73a16f23
Richard765 <richardexposito04@gmail.com>
2024-10-23 18:32:27 +0200

    Act 3_3_4

--- Programacion/Bloque1/Unidad3/ACT3_3/ACT3_3_4.java
+++ Programacion/Bloque1/Unidad3/ACT3_3/ACT3_3_4.java
@@ -1,14 +1,47 @@
 public class ACT3_3_4 {
 	public static void main(String[] args) {
-		final int num = 10, MIN = 10, MAX = 99;
-		int [][] matriu = new int [num][num];
+		final int num = 3, MIN = 10, MAX = 99;
+		int [][] matriz = new int [num][num];
+		int mayor = MIN, menor = MAX, contar = 0, suma = 0;
+		double media = 0;
 		
-		for (int i = 0; i < matriu.length; i++) {
-			for (int j = 0; j < matriu[i].length; j++) {
-				matriu[i][j] = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
-				System.out.print(matriu[i][j] + " ");
+		//Dar valor a cada array
+		System.out.println();
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				matriz[i][j] = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
+				System.out.print(matriz[i][j] + " ");
 			}
 		System.out.println();
 		}
+		
+		//Buscar el numero mayor
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if (matriz[i][j] > mayor) {
+					mayor = matriz[i][j];
+				}
+			}
+		}
+		System.out.println("\nEl numero mas alto es: " + mayor);
+		
+		//Buscar el numero menor
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if (menor > matriz[i][j]) {
+					menor = matriz[i][j];
+				}
+			}
+		}
+		System.out.println("\nEl numero mas bajo es: " + menor);
+		
+		//Buscar la media
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				contar++;
+				suma = matriz[i][j] + suma;
+			}
+		}
+		System.out.println("\nLa media es: " + (media = (double) suma / contar));
 	}
 }
