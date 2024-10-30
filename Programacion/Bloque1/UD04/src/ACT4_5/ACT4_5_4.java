c1e16172aa2332f8a510758508e7d86685b7b1e2
Richard765 <richardexposito04@gmail.com>
2024-10-30 20:09:23 +0100

    UD04 con sus actividades

---
+++ Programacion/Bloque1/UD04/src/ACT4_5/ACT4_5_4.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ACT4_5;
+
+/**
+ *
+ * @author Alumnat
+ */
+public class ACT4_5_4 {
+    
+}
--- Programacion/Bloque1/Unidad4/ACT4_0/ACT4_0_1.java
+++ Programacion/Bloque1/Unidad4/ACT4_0/ACT4_0_1.java
@@ -1,110 +1,97 @@
 import java.util.Scanner;
 
-public class ACT4_0_01 {
-/**
- *
- * @author Tomeu Vives
- */
+public class ACT4_0_1 {
     public static void main(String[] args) {
-        int numElementos, min, max, total, suma;
-		int[][] matriz;
-		float media;
+        int numElementos, min, max;
+        int[][] matriz;
+        float media;
+
+        numElementos = LeerElementos("Introduce el numero de elementos de la matriz: ");
+        matriz = new int[numElementos][numElementos];
+
+        matriz = LeerMatriz(matriz);
+
+        MostrarMatriz(matriz);
+
+        min = CalcularMin(matriz);
 		
-		// Lee la longitud de la matriz
-		numElementos = LeerElementos("Introduexi el núm elements de la matriu: ");
-
-        matriz = new int [numElementso] [numElementos];
-
-        // Lee los datos con el teclado
-		matriz = LeerMatriz(matriz);
-
-        // Mostra la matriz
-		MostrarMatriz(matriz);
-			
-		// Calcula el min
-		min = CalcularMin(matriz);
-			
-		// Calcula el max
         max = CalcularMax(matriz);
 		
-		// Calcula la media
-		media = CalcularMedia(matriz);
-		
-		// Mostrar resultados
-		MostrarResultados(min, max, media);
-    }
-		
-	// Mètodes del programa:		
-	public static int LeerElementos(String text) {
-		Scanner scanner = new Scanner(System.in);
-		int medida;
-		System.out.print(text);
-		mida = scanner.nextInt();
-		
-		//scanner.close();
-		return medida;
-	};
-	
-	public static int[][] LlegeixMatriu(int[][] matriu) {
-		Scanner scanner = new Scanner(System.in);
-		for (int i = 0; i < matriu.length; i++)
-			for (int j = 0; j < matriu[0].length; j++) {
-				System.out.print("matriu[" + i + "][" + j + "]: ");
-				matriu[i][j] = scanner.nextInt();
-			}
-		
-		//scanner.close();
-		return matriu;
-		
-	}
-	
-	public static void MostraMatriu(int[][] matriu) {
-		System.out.println("La matriu és:");
-		for (int[] array : matriu) {
-			for (int valor : array) 
-				System.out.print( valor + " " );
-			System.out.println();
-		}
-	}
-	
-	public static int CalculaMin(int[][] matriu) {
-		int min = matriu[0][0];
-		
-		for (int[] array : matriu)
-			for (int valor : array)
-				if (valor < min )
-					min = valor;
-				
-		return min;
-	}
-	
-	public static int CalculaMax(int[][] matriu) {
-		int max = matriu[0][0];
-		
-		for (int[] array : matriu)
-			for (int valor : array)
-				if (valor > max)
-					max = valor;
-				
-		return max;
-	}
-	
-	public static float CalculaMitjana(int[][] matriu) {
-		int total = 0; int suma = 0;
-		
-		for (int[] array : matriu)
-			for (int valor : array) {
-				suma += valor;
-				total++;
-			}
-		return (float) suma / total;
-	}
-	
-	public static void MostraResultats(int min, int max, float mitjana) {
-		System.out.println("Els resultats són:");
-		System.out.println("\tMàxim: " + max);
-		System.out.println("\tMínim: " + min);
-		System.out.println("\tMitjana: " + mitjana);
-	}
+        media = CalcularMedia(matriz);
 
+        MostrarResultados(min, max, media);
+    }
+
+    public static int LeerElementos(String text) {
+        Scanner scanner = new Scanner(System.in);
+        int medida;
+        System.out.print(text);
+        medida = scanner.nextInt();
+
+        return medida;
+    }
+
+    public static int[][] LeerMatriz(int[][] matriz) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < matriz.length; i++)
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print("matriz[" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        return matriz;
+    }
+
+    public static void MostrarMatriz(int[][] matriz) {
+        System.out.println("La matriz es:");
+        for (int[] array : matriz) {
+            for (int valor : array) {
+                System.out.print(valor + " ");
+			}
+            System.out.println();
+        }
+    }
+
+    public static int CalcularMin(int[][] matriz) {
+        int min = matriz[0][0];
+        for (int[] array : matriz) {
+            for (int valor : array) {
+                if (valor < min) {
+                    min = valor;
+				}
+			}
+		}
+        return min;
+    }
+
+    public static int CalcularMax(int[][] matriz) {
+        int max = matriz[0][0];
+        for (int[] array : matriz) {
+            for (int valor : array) {
+                if (valor > max) {
+                    max = valor;
+				}
+			}
+		}
+        return max;
+    }
+
+    public static float CalcularMedia(int[][] matriz) {
+        int total = 0;
+        int suma = 0;
+        for (int[] array : matriz) {
+            for (int valor : array) {
+                suma += valor;
+                total++;
+            }
+		}
+        return (float) suma / total;
+    }
+
+    public static void MostrarResultados(int min, int max, float media) {
+		System.out.println();
+        System.out.println("Los resultados son:");
+        System.out.println("Numero mas alto: " + max);
+        System.out.println("Numero mas bajo: " + min);
+        System.out.println("Media de todos los numeros: " + media);
+    }
 }
