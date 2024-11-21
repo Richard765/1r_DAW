a63b44d8d32871901bc649ea2041bf73e7dc83f2
unknown <richardexposito04@gmail.com>
2024-11-21 16:45:34 +0100

    Ejercicio 1 terminado

--- Programacion/Bloque1/UD04/src/ACT4_6/ACT4_6_B.java
+++ Programacion/Bloque1/UD04/src/ACT4_6/ACT4_6_B.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ACT4_6;
 
-/**
- *
- * @author Alumnat
- */
+import ACT4_1.UtilitatsArrays;
+import ACT4_3.UtilitatsConsola;
+import ACT4_3.UtilitatsMatrius;
+
 public class ACT4_6_B {
+    static int SIMBOLO_MATAR = 1;
+    static int SIMBOLO_TORRE = 9;
+    static int[][] tablero;
+    static int[] torre; // x,y del torre
+    
+    
+    public static void main(String[] args) {
+        final int NTABLERO = UtilitatsConsola.llegirSencer("Medidas del tablero: ");
+        tablero = UtilitatsMatrius.generaMatriu(NTABLERO, 0, 0);  // matriu NTAULERxNTAULER
+        torre = UtilitatsArrays.generaArray(2,0, tablero.length-1);     // (x,y) del cuc
+
+        while () {
+                colocaTorre(tablero, torre);
+                utilizaTablero(tablero, torre);
+                matarTorre(tablero, torre);
+
+        } 
+    }
+
+    public static void colocaTorre(int[][] tauler, int[] cuc) {
+        torre = UtilitatsArrays.generaArray(2, 0, tablero.length - 1);
+    }
+    
+    public static void utilizaTablero(int[][] tauler, int[] torre) {
+
+    }
+        
+    public static void matarTorre(int[][] tauler, int[] torre) {
+
+    }
     
 }
+
--- Programacion/Bloque1/UD04/src/ACT4_6/ACT4_6_C.java
+++ Programacion/Bloque1/UD04/src/ACT4_6/ACT4_6_C.java
@@ -1,11 +1,4 @@
 package ACT4_6;
 
-import ACT4_1.UtilitatsArrays;
-import ACT4_3.UtilitatsConsola;
-import ACT4_3.UtilitatsMatrius;
-
 public class ACT4_6_C {
-    int[][] tablero;
-    final int NTABLERO = UtilitatsConsola.llegirSencer("Medida del tablero: ");
-    tablero = UtilitatsMatrius.generaMatriu(NTABLERO, 0, 0); // matriz NTABLEROxNTABLERO
 }
