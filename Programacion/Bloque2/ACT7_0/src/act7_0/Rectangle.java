0d8dffe016158967fafa71d774aeec77cf69084f
unknown <richardexposito04@gmail.com>
2025-02-04 16:51:05 +0100

    Programacion

---
+++ Programacion/Bloque2/ACT7_0/src/act7_0/Rectangle.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package act7_0;
+
+/**
+ *
+ * @author Richard
+ */
+public class Rectangle extends FiguraGeometrica {
+    private int costat1;
+    private int costat2;
+    
+}
